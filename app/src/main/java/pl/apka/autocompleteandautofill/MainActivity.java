3e956ea28144f9f74c298cf6cb16d3ecd0e74f53
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-02-21 14:12:38 +0100

    autocomplete textview

--- app/src/main/java/pl/apka/autocompleteandautofill/MainActivity.java
+++ app/src/main/java/pl/apka/autocompleteandautofill/MainActivity.java
@@ -2,6 +2,8 @@ package pl.apka.autocompleteandautofill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] friends = {"Jon","John","Jolene","Jane","Sarah","Joe","Roma","Greg","Roman"};
+
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,friends);
+
+        autoCompleteTextView.setAdapter(arrayAdapter);
+
     }
 }
